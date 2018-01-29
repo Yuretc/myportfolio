a73bb39c9ccf0881f97dbdf80b41d247ae202791
Yuretc <34270317+Yuretc@users.noreply.github.com>
2018-01-29 15:49:23 +0500

    Add files via upload
    Изменил файловую структуру

---
+++ Scripts/JS-test.js
@@ -0,0 +1,73 @@
+// var ourPlanetName = "Земля";
+// var userName = "Петя";
+// admin = name;
+// console.log(userName + " живёт на планете " + ourPlanetName);
+
+// var vf = 5;
+// var ns = "hello, world!";
+// var boo = true;
+// var nu = null;
+// var ub
+// var ob = {
+//     name: "Vasya",
+//     family: "Petrov",
+//     age: 12
+// };
+
+// console.log(typeof vf);
+// console.log(typeof ns);
+// console.log(typeof boo);
+// console.log(typeof nu);
+// console.log(typeof ub);
+// console.log(typeof ob);
+// console.log(ob.name);
+// console.log(ob.family);
+// console.log(ob.age);
+
+// var apples = "2";
+// var oranges = "4";
+// console.log(+apples + + oranges);
+
+// var a, b, c;
+// a = b = c = 2 + 2;
+// a = 5
+// b = 2
+// c = 3 - ((b + c) + 6)
+// console.log(a, b, c)
+// console.log(a = c % b)
+
+// var a, b, i, k;
+// i = 1, k = 1;
+// a = ++i;
+// b = k++;
+// console.log(a, b);
+// console.log(i, k);
+
+// var i = 1;
+// console.log(2 * ++i, 4);
+
+// i = 1;
+// console.log(2 * i++, 2);
+// console.log(i, 2);
+// console.log(2 * i++, 4);
+// console.log(i, 3);
+
+// i = 1;
+// console.log(2 * i, 2);
+// i++;
+// console.log(i, 2);
+
+// var a = (5, 6, 8, 9);
+// console.log(a);
+
+// Сравнения чисел и переменных
+
+var a = '22Пр', b = '22При', c, d = true;
+c = a > b
+console.log("a=", a, "b=", b);
+console.log("c=", c, "d=", d);
+console.log(c == d);
+
+// console.log( null > 0 );
+// console.log( null == 0 );
+// console.log(null >= 0);
