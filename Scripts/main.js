9036449a00419866dc4d546c9ff3c9b0317e3fe6
Pinegin Yuriy <che_zmoor@mail.ru>
2018-11-18 14:36:57 +0500

    Доработка страницы solar studio

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,9 @@
 (function($) {
   $('.js-nav-menu-toggle').on('click', function() {
     $(this).parents('.navigation-menu').toggleClass('navigation-menu--open');
+    $('.navigation-menu__toggle').toggleClass('navigation-menu__toggle--open')
   });
+  
   
   // $('html').on('click', function(e) {
   //   if(!$(e.target).closest('.js-nav-menu').length &&
