8bdb150a06aca214d62455de4aeeaee96a2570b0
Pinegin Yuriy <che_zmoor@mail.ru>
2018-12-05 07:06:47 +0500

    Навигационное меню анимация

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,16 +1,9 @@
 (function($) {
   $('.js-nav-menu-toggle').on('click', function() {
-    $(this).parents('.navigation-menu').toggleClass('navigation-menu--open');
+    $(this).parents('.navigation-menu').toggleClass('navigation-menu--closed');
     $('.navigation-menu__toggle').toggleClass('navigation-menu__toggle--open')
   });
-  
-  
-  // $('html').on('click', function(e) {
-  //   if(!$(e.target).closest('.js-nav-menu').length &&
-  //     ($('.js-nav-menu').hasClass('navigation-menu--open'))) {
-  //       $('.js-nav-menu').removeClass('navigation-menu--open');
-  //   }
-  // });
+
 })(jQuery);
 
 // mainNavMenuBurger.addEventListener("click", function () {
